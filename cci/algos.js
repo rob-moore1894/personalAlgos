7d868b03af2c761c94e5bba57a2e534e65ca374b
Rob Moore <moore.re@gmail.com>
2021-03-02 12:07:53 -0600

    Added doSearch function to show Binary Search

--- cci/algos.js
+++ cci/algos.js
@@ -159,3 +159,31 @@ console.log(computerGuess);
 
 userGuess(50, computerGuess);
 userGuess(8, computerGuess);
+
+const doSearch = (array, targetValue) => {
+    let min = 0;
+    let max = array.length - 1;
+    let guess;
+
+    while(max >= min){
+        guess = Math.floor((min + max)/2);
+        console.log(`Index = ${guess}`, `Value of array at ${guess} = ${array[guess]}`)
+        if(array[guess] === targetValue){
+            return guess;
+        }
+        else if(array[guess] < targetValue){
+            console.log(`${array[guess]} is too low`)
+            min = guess + 1;
+        }
+        else {
+            console.log(`${array[guess]} is too high`)
+            max = guess - 1;
+        }
+    }
+    return -1; 
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 73);
+console.log(`Found prime at index ${result}`);
